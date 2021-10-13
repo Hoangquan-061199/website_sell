0c9f17720cf25812be117bb47f6c7c3b956a5dd6
hoangquan-061199 <hoangquan061199@gmail.com>
2021-10-13 18:19:06 +0700

    update js

--- asset/js/main.js
+++ asset/js/main.js
@@ -41,6 +41,304 @@ $(document).ready(() => {
   const prevSuggest = $(".prev-suggest-btn");
   const nextSuggest = $(".next-suggest-btn");
 
+  var listPhones = [
+    {
+      name: "Samsung Galaxy Note 21",
+      priceNew: "29000000",
+      priceLast: "30500000",
+      imgSrc:
+        "https://bizweb.dktcdn.net/thumb/medium/100/429/689/products/dien-thoai-samsung-galaxy-s21-ultra-5g-256g-g998b-bac-h0l175-b6ac0e17-afc8-42a9-bb4f-aecaf81ad7e2.jpg?v=1623565400000",
+    },
+    {
+      name: "Iphone 12 Mini New",
+      priceNew: "26000000",
+      priceLast: "",
+      imgSrc:
+        "https://bizweb.dktcdn.net/thumb/medium/100/429/689/products/apple-iphone-12-mini-2-0340d148-dd79-40b3-b8ad-95ed589c7b5a.png?v=1623564588000",
+    },
+    {
+      name: "Iphone 12 Pro Max Like New",
+      priceNew: "22000000",
+      priceLast: "32000000",
+      imgSrc:
+        "https://bizweb.dktcdn.net/thumb/medium/100/429/689/products/iphone-12-pro-max-xanh-561e05b7-eed5-40c8-9065-49b5a1cea423.jpg?v=1623565125000",
+    },
+    {
+      name: "Iphone XR - Vàng Chính Hãng VN/A",
+      priceNew: "8000000",
+      priceLast: "10000000",
+      imgSrc:
+        "https://bizweb.dktcdn.net/thumb/medium/100/429/689/products/iphone-xr-mau-vang-didongviet-1-1-2.jpg?v=1623561749000",
+    },
+    {
+      name: "Xiaomi Mi 10T Pro 5G",
+      priceNew: "10900000",
+      priceLast: "12500000",
+      imgSrc:
+        "https://bizweb.dktcdn.net/thumb/medium/100/429/689/products/xiaomi-mi-10t-pro.jpg?v=1623565104000",
+    },
+    {
+      name: "Iphone 12 Hàng Quốc Tế",
+      priceNew: "26000000",
+      priceLast: "32000000",
+      imgSrc:
+        "https://bizweb.dktcdn.net/thumb/medium/100/429/689/products/apple-iphone-12-mini-5.png?v=1623565259000",
+    },
+  ];
+
+  var listFashions = [
+    {
+      name: "Kappa giày thể thao Nam/Nữ 001",
+      priceNew: "690000",
+      priceLast: "1790000",
+      imgSrc:
+        "https://bizweb.dktcdn.net/thumb/medium/100/429/689/products/304itd0-a0u-vuong-a0954654-0a43-4a9c-a026-5786147d0bf0.jpg?v=1623808988000",
+    },
+    {
+      name: "Kappa giày thể thao Nam/Nữ 31169FW 001",
+      priceNew: "690000",
+      priceLast: "1790000",
+      imgSrc:
+        "https://bizweb.dktcdn.net/thumb/medium/100/429/689/products/31169fw-001-1.jpg?v=1623808992000",
+    },
+    {
+      name: "Kappa giày thể thao Nam/Nữ 3116Z2W A04",
+      priceNew: "690000",
+      priceLast: "1790000",
+      imgSrc:
+        "https://bizweb.dktcdn.net/thumb/medium/100/429/689/products/3116z2w-a04-vuong.jpg?v=1623809101000",
+    },
+    {
+      name: "Kappa giày thể thao Nam/Nữ 3116Z3W 001",
+      priceNew: "790000",
+      priceLast: "1790000",
+      imgSrc:
+        "https://bizweb.dktcdn.net/thumb/medium/100/429/689/products/3116z3w-001-vuong.jpg?v=1623809190000",
+    },
+    {
+      name: "Kappa giày thể thao Nam/Nữ 3116Z3W 001",
+      priceNew: "790000",
+      priceLast: "1790000",
+      imgSrc:
+        "https://bizweb.dktcdn.net/thumb/medium/100/429/689/products/3116z3w-001-vuong.jpg?v=1623809190000",
+    },
+    {
+      name: "Kappa giày thể thao Nam/Nữ 3116Z3W 001",
+      priceNew: "790000",
+      priceLast: "1790000",
+      imgSrc:
+        "https://bizweb.dktcdn.net/thumb/medium/100/429/689/products/3116z3w-001-vuong.jpg?v=1623809190000",
+    },
+  ];
+
+  var listAppliances = [
+    {
+      name: "Nồi cơm điện thông minh Bear SUBE015 DFB-B20A1",
+      priceNew: "1190000",
+      priceLast: "",
+      imgSrc:
+        "https://bizweb.dktcdn.net/thumb/medium/100/429/689/products/noi-com-dien-thong-minh-bear-sube015-dfb-b20a1-1620449971.jpg?v=1623599778000",
+    },
+    {
+      name: "Máy xay ăn dặm 0.3L Bear QSJ-A01F2 (GR-B03V2)",
+      priceNew: "890000",
+      priceLast: "1200000",
+      imgSrc:
+        "https://bizweb.dktcdn.net/thumb/medium/100/429/689/products/may-xay-an-dam-03l-bear-qsj-a01f2-gr-b03v2-12062021.png?v=1623599484000",
+    },
+    {
+      name: "Máy xay ăn dặm 3 cối Bear LLJ-C04J1 (GR-B03V1)",
+      priceNew: "1190000",
+      priceLast: "",
+      imgSrc:
+        "https://bizweb.dktcdn.net/thumb/medium/100/429/689/products/may-xay-an-dam-3-coi-bear-llj-c04j1-gr-b03v1-12062021.png?v=1623599225000",
+    },
+    {
+      name: "Nồi nấu chậm 0,8L Bear SUBE001 - Bản Quốc Tế",
+      priceNew: "550000",
+      priceLast: "",
+      imgSrc:
+        "https://bizweb.dktcdn.net/thumb/medium/100/429/689/products/noi-nau-cham-08l-bear-sube001-ban-quoc-te-11012021.jpg?v=1623598983000",
+    },
+    {
+      name: "Nồi cơm điện thông minh Bear SUBE015 DFB-B20A1",
+      priceNew: "1190000",
+      priceLast: "",
+      imgSrc:
+        "https://bizweb.dktcdn.net/thumb/medium/100/429/689/products/noi-com-dien-thong-minh-bear-sube015-dfb-b20a1-1620449971.jpg?v=1623599778000",
+    },
+    {
+      name: "Nồi cơm điện thông minh Bear SUBE015 DFB-B20A1",
+      priceNew: "1190000",
+      priceLast: "",
+      imgSrc:
+        "https://bizweb.dktcdn.net/thumb/medium/100/429/689/products/noi-com-dien-thong-minh-bear-sube015-dfb-b20a1-1620449971.jpg?v=1623599778000",
+    },
+    {
+      name: "Nồi cơm điện thông minh Bear SUBE015 DFB-B20A1",
+      priceNew: "1190000",
+      priceLast: "",
+      imgSrc:
+        "https://bizweb.dktcdn.net/thumb/medium/100/429/689/products/noi-com-dien-thong-minh-bear-sube015-dfb-b20a1-1620449971.jpg?v=1623599778000",
+    },
+    {
+      name: "Nồi cơm điện thông minh Bear SUBE015 DFB-B20A1",
+      priceNew: "1190000",
+      priceLast: "",
+      imgSrc:
+        "https://bizweb.dktcdn.net/thumb/medium/100/429/689/products/noi-com-dien-thong-minh-bear-sube015-dfb-b20a1-1620449971.jpg?v=1623599778000",
+    },
+  ];
+
+  // get value array listPhones
+  for (let i = 0; i < listPhones.length; i++) {
+    let html = `
+      <div class="item__product">
+        <img src="${listPhones[i].imgSrc}" alt="">
+        <span class="item__product-name">${listPhones[i].name}</span>
+        <div class="product-price">
+            <p class="item__product-buys-new">
+                ${listPhones[i].priceNew}
+                <span>đ</span>
+            </p>
+            <p class="item__product-buys-last">
+                ${listPhones[i].priceLast}
+                <span>đ</span>
+            </p>
+        </div>
+        <button class="item__product-cart" title="Thêm vào giỏ hàng">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                class="bi bi-cart2" viewBox="0 0 16 16">
+                <path
+                    d="M0 2.5A.5.5 0 0 1 .5 2H2a.5.5 0 0 1 .485.379L2.89 4H14.5a.5.5 0 0 1 .485.621l-1.5 6A.5.5 0 0 1 13 11H4a.5.5 0 0 1-.485-.379L1.61 3H.5a.5.5 0 0 1-.5-.5zM3.14 5l1.25 5h8.22l1.25-5H3.14zM5 13a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0zm9-1a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0z" />
+            </svg>
+        </button>
+        <div class="product-icon">
+            <div class="icon-heart" title="Thêm vào yêu thích">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                    class="bi bi-heart" viewBox="0 0 16 16">
+                    <path
+                        d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z" />
+                </svg>
+            </div>
+            <div class="icon-compare" title="Thêm vào so sánh">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                    class="bi bi-bar-chart-fill" viewBox="0 0 16 16">
+                    <path
+                        d="M1 11a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v3a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1v-3zm5-4a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v7a1 1 0 0 1-1 1H7a1 1 0 0 1-1-1V7zm5-5a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1h-2a1 1 0 0 1-1-1V2z" />
+                </svg>
+            </div>
+        </div>
+        <div class="product-discount">
+            Giảm
+            <p>5%</p>
+        </div>
+      </div>
+    `;
+
+    sectionContentPhone.append(html);
+  }
+
+  // get value array listFashions
+  for (let i = 0; i < listFashions.length; i++) {
+    var html = `
+      <div class="item__product">
+        <img src="${listFashions[i].imgSrc}"
+            alt="">
+        <span class="item__product-name">${listFashions[i].name}</span>
+        <div class="product-price">
+            <p class="item__product-buys-new">
+                ${listFashions[i].priceNew}
+                <span>đ</span>
+            </p>
+            <p class="item__product-buys-last">
+                ${listFashions[i].priceLast}
+                <span>đ</span>
+            </p>
+        </div>
+        <button class="item__product-cart" title="Thêm vào giỏ hàng">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                class="bi bi-cart2" viewBox="0 0 16 16">
+                <path
+                    d="M0 2.5A.5.5 0 0 1 .5 2H2a.5.5 0 0 1 .485.379L2.89 4H14.5a.5.5 0 0 1 .485.621l-1.5 6A.5.5 0 0 1 13 11H4a.5.5 0 0 1-.485-.379L1.61 3H.5a.5.5 0 0 1-.5-.5zM3.14 5l1.25 5h8.22l1.25-5H3.14zM5 13a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0zm9-1a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0z" />
+            </svg>
+        </button>
+        <div class="product-icon">
+            <div class="icon-heart" title="Thêm vào yêu thích">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                    class="bi bi-heart" viewBox="0 0 16 16">
+                    <path
+                        d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z" />
+                </svg>
+            </div>
+            <div class="icon-compare" title="Thêm vào so sánh">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                    class="bi bi-bar-chart-fill" viewBox="0 0 16 16">
+                    <path
+                        d="M1 11a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v3a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1v-3zm5-4a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v7a1 1 0 0 1-1 1H7a1 1 0 0 1-1-1V7zm5-5a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1h-2a1 1 0 0 1-1-1V2z" />
+                </svg>
+            </div>
+        </div>
+        <div class="product-discount">
+            Giảm
+            <p>61%</p>
+        </div>
+      </div>
+    `;
+
+    sectionContentStyle.append(html);
+  }
+
+  // get value array listFashions
+  for (let i = 0; i < listAppliances.length; i++) {
+    var html = `
+        <div class="item__product">
+          <img src="${listAppliances[i].imgSrc}"
+              alt="">
+          <span class="item__product-name">${listAppliances[i].name}</span>
+          <div class="product-price">
+              <p class="item__product-buys-new">
+                  ${listAppliances[i].priceNew}
+                  <span>đ</span>
+              </p>
+              <p class="item__product-buys-last">
+                  ${listAppliances[i].priceLast}
+                  <span>đ</span>
+              </p>
+          </div>
+          <button class="item__product-cart" title="Thêm vào giỏ hàng">
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                  class="bi bi-cart2" viewBox="0 0 16 16">
+                  <path
+                      d="M0 2.5A.5.5 0 0 1 .5 2H2a.5.5 0 0 1 .485.379L2.89 4H14.5a.5.5 0 0 1 .485.621l-1.5 6A.5.5 0 0 1 13 11H4a.5.5 0 0 1-.485-.379L1.61 3H.5a.5.5 0 0 1-.5-.5zM3.14 5l1.25 5h8.22l1.25-5H3.14zM5 13a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0zm9-1a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0z" />
+              </svg>
+          </button>
+          <div class="product-icon">
+              <div class="icon-heart" title="Thêm vào yêu thích">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                      class="bi bi-heart" viewBox="0 0 16 16">
+                      <path
+                          d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z" />
+                  </svg>
+              </div>
+              <div class="icon-compare" title="Thêm vào so sánh">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                      class="bi bi-bar-chart-fill" viewBox="0 0 16 16">
+                      <path
+                          d="M1 11a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v3a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1v-3zm5-4a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v7a1 1 0 0 1-1 1H7a1 1 0 0 1-1-1V7zm5-5a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1h-2a1 1 0 0 1-1-1V2z" />
+                  </svg>
+              </div>
+          </div>
+          <div class="product-discount">
+              Giảm
+              <p>61%</p>
+          </div>
+        </div>
+      `;
+
+    sectionContentAppliances.append(html);
+  }
+
   // modal
   $(".myModal").on("shown.bs.modal", () => {
     $(".myInput").focus();
